76f63ce435c78818acb83c7f51af9bb684364333
Samuel Elliott <sam@lenary.co.uk>
2009-05-14 21:37:46 +0100

    New Colour Scheme because I felt the old one was too dark. Closes GH-5
    This uses far lighter colours for a more 'github'y look. I have also removed the
    unminified  source of  jQuery,  and added  in  some scripts  so  that the  pages
    (should) render correctly in IE.
    
    All    colour   declarations    in   the    css   have    been   extracted    to
    `public/src/colours.css` so  that it  is easier  for someone  to make  their own
    colour scheme.
    
    Please do take the time to play  with this in internet explorer, because I don't
    use a mac so i  can't test in any of the IE versions. Just  open a new ticket or
    add a comment to an open one if there are any problems. Please Include a link to
    a screenshot so  that we can see what's wrong.  The Perfect Renderings (provided
    by Safari 4) can be seen here (the first four images):
    
    http://www.flickr.com/photos/24295828@N03/sets/72157618065085813/

--- public/src/ginatra.js
+++ public/src/ginatra.js
@@ -4,6 +4,14 @@ $(document).ready(function (){
   $(".commit-committer tr:first-child td").addClass("first-no-border"); 
   $(".commit-files tr:first-child td").addClass("first-no-border"); 
   $(".commit-log tr:first-child td").addClass("first-no-border"); 
+  $(".repo-list dl dt:first-child").addClass("first-no-border"); 
+  $(".CodeRay .c:before").addClass("before-c");
+  $(".CodeRay .ins .ins:after").addClass("before-line");
+  $(".CodeRay .ins .ins:before").addClass("before-line");
+  $(".CodeRay .del .del:after").addClass("before-line");
+  $(".CodeRay .del .del:before").addClass("before-line");
+  $(".CodeRay .chg .chg:before").addClass("before-line");
+  
   $(".commit-tree li ul").hide();
   $(".commit-tree li a").click(function (){
     $(this).next("ul").toggle();
