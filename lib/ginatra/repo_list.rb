3b19a0eaa4eaa8670d5afc80529e799921b9acb6
Jonathan Stott <jonathan.stott@gmail.com>
2009-07-25 23:11:30 +0100

    Refactored RepoList to allow for list refreshes.

--- lib/ginatra/repo_list.rb
+++ lib/ginatra/repo_list.rb
@@ -5,19 +5,43 @@ module Ginatra
     attr_accessor :list
     def initialize
 
-      self.list =  Dir.entries(Ginatra::Config.git_dir).
-                   delete_if{ |e| Ginatra::Config.ignored_files.include?(e) }.
-                   map!{ |e| File.expand_path(e, Ginatra::Config.git_dir) }.
-                   map!{ |e| Repo.new(e) }
+      self.list = []
+      self.refresh
     end
     
     # For convinience
     def self.list
+      self.instance.refresh
       self.instance.list
     end
-    
+
+    def refresh
+      Dir.entries(Ginatra::Config.git_dir).
+        delete_if{ |e| Ginatra::Config.ignored_files.include?(e) }.
+        each { |e| add(e) }
+    end
+
+    def add(e, path = File.expand_path(e, Ginatra::Config.git_dir))
+      unless self.has_repo?(e)
+        list << Repo.new(path)
+      end
+    end
+
+    def has_repo?(local_param)
+      list.find { |r| r.param == local_param } ? true : false
+    end
+
+    def find(local_param)
+      if repo = list.find { |r| r.param == local_param }
+        repo
+      else
+        refresh
+        list.find { |r| r.param == local_param }
+      end
+    end
+
     def self.find(local_param)
-      list.find { |r| r.param == local_param }
+      self.instance.find(local_param)
     end
 
     def self.method_missing(sym, *args, &block)
