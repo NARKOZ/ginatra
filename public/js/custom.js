085e61ba2463ae5f87787d7ebf7b365c4f1bbee4
Nihad Abbasov <narkoz.2008@gmail.com>
2013-04-15 12:58:27 +0500

    ability to filter repos by name and description

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,9 +1,20 @@
 $(function() {
-  $('.js-nav').click(function(){
+  $('.js-nav').click(function() {
     location.href = $(this).data('href');
   });
 
   $('#pjax-container').pjax('#js-tree a, #js-tree-nav a').on('pjax:send', function(){
     $('#loader').show();
+  });
+
+  // filter repositories
+  $('.js-filter-query').on('keyup', function(e) {
+    var regexp = new RegExp($(this).val(), 'i'),
+        $repolist = $('.js-repolist li');
+
+    $repolist.hide();
+    $repolist.filter(function() {
+      return regexp.test($(this).text());
+    }).show();
   });
 });
