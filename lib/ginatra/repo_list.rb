31f6e5dd341942c22df051909c4c54bfbdcd014f
James Strocel <james.strocel@gmail.com>
2015-06-23 11:38:44 -0700

    cleaned up repo_list.rb

--- lib/ginatra/repo_list.rb
+++ lib/ginatra/repo_list.rb
@@ -14,45 +14,51 @@ module Ginatra
     # It returns what refresh returns.
     def initialize
       self.list = []
-      self.refresh
+      refresh
     end
 
     # The preferred way to access the list publicly.
     #
     # @return [Array<Ginatra::Repo>] a list of ginatra repos.
     def self.list
-      self.instance.refresh
-      self.instance.list
+      instance.refresh
+      instance.list
     end
 
-    # searches through the configured directory globs to find all the repositories
-    # and adds them if they're not already there.
+    # searches through the configured directory globs to find all
+    #  the repositories and adds them if they're not already there.
     def refresh
       list.clear
 
-      Ginatra.load_config["git_dirs"].map do |git_dir|
-        if Dir.exist?(git_dir.chop)
-          dirs = Dir.glob(git_dir).sort
-        else
-          dir = File.expand_path("../../../#{git_dir}", __FILE__)
-          dirs = Dir.glob(dir).sort
-        end
-
-        dirs = dirs.select {|f| File.directory? f }
-        dirs.each {|d| add(d) }
+      Ginatra.load_config['git_dirs'].map do |git_dir|
+        parse_dir(git_dir)
       end
 
       list
     end
 
-    # adds a Repo corresponding to the path it found a git repo at in the configured
-    # globs. Checks to see that it's not there first
+    # parses the directories for the refresh method.
+
+    def parse_dir(git_dir)
+      if Dir.exist?(git_dir.chop)
+        dirs = Dir.glob(git_dir).sort
+      else
+        dir = File.expand_path("../../../#{git_dir}", __FILE__)
+        dirs = Dir.glob(dir).sort
+      end
+
+      dirs = dirs.select { |f| File.directory? f }
+      dirs.each { |d| add(d) }
+    end
+
+    # adds a Repo corresponding to the path it found a git repo at in the
+    # configured globs. Checks to see that it's not there first
     #
     # @param [String] path the path of the git repo
     # @param [String] param the param of the repo if it differs,
     #   for looking to see if it's already on the list
-    def add(path, param=File.split(path).last)
-      unless self.has_repo?(param)
+    def add(path, param = File.split(path).last)
+      unless self.repo?(param)
         begin
           list << Repo.new(path)
         rescue Rugged::RepositoryError
@@ -68,7 +74,7 @@ module Ginatra
     # @param [String] local_param param to check.
     #
     # @return [true, false]
-    def has_repo?(local_param)
+    def repo?(local_param)
       !list.find { |r| r.param == local_param }.nil?
     end
 
@@ -79,12 +85,13 @@ module Ginatra
     #
     # @return [Ginatra::Repo] the repository corresponding to that param.
     def find(local_param)
-      if repo = list.find { |r| r.param == local_param }
+      repo = list.find { |r| r.param == local_param }
+      if repo
         repo
       else
         refresh
         repo = list.find { |r| r.param == local_param }
-        raise Ginatra::RepoNotFound if repo.nil?
+        fail Ginatra::RepoNotFound if repo.nil?
         repo
       end
     end
@@ -93,7 +100,7 @@ module Ginatra
     #
     # @see Ginatra::RepoList#find
     def self.find(local_param)
-      self.instance.find(local_param)
+      instance.find(local_param)
     end
 
     # allows missing methods to cascade to the instance,
--- spec/ginatra/repo_list_spec.rb
+++ spec/ginatra/repo_list_spec.rb
@@ -14,12 +14,12 @@ describe Ginatra::RepoList do
     expect(repo_list).to include(repo)
   end
 
-  it "has_repo? works for existing repo" do
-    expect(Ginatra::RepoList.instance.has_repo?('test')).to be true
+  it "repo? works for existing repo" do
+    expect(Ginatra::RepoList.instance.repo?('test')).to be true
   end
 
-  it "has_repo? works for non-existant repo" do
-    expect(Ginatra::RepoList.instance.has_repo?('bad-test')).to be false
+  it "repo? works for non-existant repo" do
+    expect(Ginatra::RepoList.instance.repo?('bad-test')).to be false
   end
 
   describe "New repos added to repo directory" do
@@ -37,7 +37,7 @@ describe Ginatra::RepoList do
 
     it "should detect new repo after refresh" do
       repo_list = Ginatra::RepoList.list # calling this should refresh the list
-      expect(Ginatra::RepoList.instance.has_repo?(@new_repo_name)).to be true
+      expect(Ginatra::RepoList.instance.repo?(@new_repo_name)).to be true
 
       new_repo = Ginatra::RepoList.find(@new_repo_name)
       expect(repo_list).to include(new_repo)
@@ -45,7 +45,7 @@ describe Ginatra::RepoList do
 
     it "should detect when a repo has been removed after refresh" do
       repo_list = Ginatra::RepoList.list # calling this should refresh the list
-      expect(Ginatra::RepoList.instance.has_repo?(@new_repo_name)).to be true
+      expect(Ginatra::RepoList.instance.repo?(@new_repo_name)).to be true
 
       new_repo = Ginatra::RepoList.find(@new_repo_name)
       expect(repo_list).to include(new_repo)
@@ -55,7 +55,7 @@ describe Ginatra::RepoList do
 
       repo_list = Ginatra::RepoList.list # refresh the repo list
 
-      expect(Ginatra::RepoList.instance.has_repo?(@new_repo_name)).to be false
+      expect(Ginatra::RepoList.instance.repo?(@new_repo_name)).to be false
       expect(repo_list).to_not include(new_repo)
     end
 
