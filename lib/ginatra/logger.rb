1e511b9b1c01ea8b432027d6a1451476fec07c2c
James Strocel <james.strocel@gmail.com>
2015-06-23 16:22:40 -0700

    Cleaning up logger.rb

--- lib/ginatra/logger.rb
+++ lib/ginatra/logger.rb
@@ -2,7 +2,9 @@ require 'logger'
 require 'fileutils'
 
 module Ginatra
+  # Encapsulates the Logger
   module Logger
+      # Locates and Prints the log file
     def logger
       Logger.logger
     end
@@ -10,17 +12,9 @@ module Ginatra
     def self.logger
       return @logger if @logger
 
-      if Ginatra.config.log_file
-        log_file = File.expand_path(Ginatra.config.log_file)
-      else
-        log_file = STDOUT
-      end
+      log_file = find_log
 
-      unless log_file == STDOUT
-        parent_dir, _separator, _filename = log_file.rpartition('/')
-        FileUtils.mkdir_p parent_dir
-        FileUtils.touch log_file
-      end
+      create_log(log_file)
 
       @logger = ::Logger.new log_file
       @logger.level = ::Logger::WARN
@@ -28,6 +22,25 @@ module Ginatra
         "[#{datetime} ##{Process.pid}] #{severity}: #{msg}\n"
       end
       @logger
+    end
+
+    # Finds log file if it is not in the instance variable
+    def self.find_log
+      if Ginatra.config.log_file
+        log_file = File.expand_path(Ginatra.config.log_file)
+      else
+        log_file = STDOUT
+      end
+      log_file
+    end
+
+    # Creates Log file
+    def self.create_log(log_file)
+      unless log_file == STDOUT
+        parent_dir, _separator, _filename = log_file.rpartition('/')
+        FileUtils.mkdir_p parent_dir
+        FileUtils.touch log_file
+      end
     end
   end
 end
