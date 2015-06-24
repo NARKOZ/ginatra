33be338e634d2753d4cc85a62930cdae286abc07
James Strocel <james.strocel@gmail.com>
2015-06-24 09:40:06 -0700

    cleaning up webby.rb

--- lib/git/webby.rb
+++ lib/git/webby.rb
@@ -10,12 +10,12 @@ require 'git/webby/extensions'
 
 # See <b>Git::Webby</b> for documentation.
 module Git
-
   # The main goal of the <b>Git::Webby</b> is implement the following useful
   # features.
   #
   # - Smart-HTTP, based on _git-http-backend_.
-  # - Authentication flexible based on database or configuration file like <tt>.htpasswd</tt>.
+  # - Authentication flexible based on database or
+  #   configuration file like <tt>.htpasswd</tt>.
   # - API to get information about repository.
   #
   # This class configure the needed variables used by application. See
@@ -54,9 +54,7 @@ module Git
   #   *receive_pack* ::
   #     Like +http.receivepack+.
   module Webby
-
     class ProjectHandler #:nodoc:
-
       # Path to git command
       attr_reader :path
 
@@ -79,11 +77,11 @@ module Git
       end
 
       def cli(command, *args)
-        %Q[#{@path} #{args.unshift(command.to_s.gsub("_","-")).compact.join(" ")}]
+        %Q[#{@path} #{args.unshift(command.to_s.gsub("_", "-")).compact.join(" ")}]
       end
 
       def run(command, *args)
-        chdir{ %x[#{cli command, *args}] }
+        chdir { %x[#{cli command, *args}] }
       end
 
       def read_file(*file)
@@ -123,7 +121,6 @@ module Git
     end
 
     class Htpasswd #:nodoc:
-
       def initialize(file)
         require 'webrick/httpauth/htpasswd'
         @handler = WEBrick::HTTPAuth::Htpasswd.new(file)
@@ -133,7 +130,7 @@ module Git
       def find(username) #:yield: password, salt
         password = @handler.get_passwd(nil, username, false)
         if block_given?
-          yield password ? [password, password[0,2]] : [nil, nil]
+          yield password ? [password, password[0, 2]] : [nil, nil]
         else
           password
         end
@@ -169,12 +166,11 @@ module Git
       private
 
       def users
-        @handler.each{|username, password| username }
+        @handler.each { |username, password| username }
       end
     end
 
     module GitHelpers #:nodoc:
-
       def git
         @git ||= ProjectHandler.new(settings.project_root, settings.git_path)
       end
@@ -187,13 +183,11 @@ module Git
       def content_type_for_git(name, *suffixes)
         content_type("application/x-git-#{name}-#{suffixes.compact.join("-")}")
       end
-
     end
 
     module AuthenticationHelpers #:nodoc:
-
       def htpasswd
-        @htpasswd ||= Htpasswd.new(git.path_to("htpasswd"))
+        @htpasswd ||= Htpasswd.new(git.path_to('htpasswd'))
       end
 
       def authentication
@@ -201,28 +195,28 @@ module Git
       end
 
       def authenticated?
-        request.env["REMOTE_USER"] && request.env["git.webby.authenticated"]
+        request.env['REMOTE_USER'] && request.env['git.webby.authenticated']
       end
 
       def authenticate(username, password)
-        checked   = [ username, password ] == authentication.credentials
+        checked   = [username, password] == authentication.credentials
         validated = authentication.provided? && authentication.basic?
         granted   = htpasswd.authenticated? username, password
-        if checked and validated and granted
-          request.env["git.webby.authenticated"] = true
-          request.env["REMOTE_USER"] = authentication.username
+        if checked && validated && granted
+          request.env['git.webby.authenticated'] = true
+          request.env['REMOTE_USER'] = authentication.username
         else
           nil
         end
       end
 
       def unauthorized!(realm = Git::Webby::info)
-        headers "WWW-Authenticate" => %(Basic realm="#{realm}")
-        throw :halt, [ 401, "Authorization Required" ]
+        headers 'WWW-Authenticate' => %(Basic realm="#{realm}")
+        throw :halt, [401, 'Authorization Required']
       end
 
       def bad_request!
-        throw :halt, [ 400, "Bad Request" ]
+        throw :halt, [400, 'Bad Request']
       end
 
       def authenticate!
@@ -230,31 +224,29 @@ module Git
         unauthorized! unless authentication.provided?
         bad_request!  unless authentication.basic?
         unauthorized! unless authenticate(*authentication.credentials)
-        request.env["REMOTE_USER"] = authentication.username
+        request.env['REMOTE_USER'] = authentication.username
       end
 
       def access_granted?(username, password)
         authenticated? || authenticate(username, password)
       end
-
     end # AuthenticationHelpers
 
     # Servers
-    autoload :HttpBackend, "git/webby/http_backend"
+    autoload :HttpBackend, 'git/webby/http_backend'
 
     class << self
-
       def config
         @config ||= {
-          :default => {
-            :project_root => "/home/git",
-            :git_path     => "/usr/bin/git"
+          default: {
+            project_root: '/home/git',
+            git_path: '/usr/bin/git'
           },
-          :http_backend => {
-            :authenticate => true,
-            :get_any_file => true,
-            :upload_pack  => true,
-            :receive_pack => false
+          http_backend: {
+            authenticate: true,
+            get_any_file: true,
+            upload_pack: true,
+            receive_pack: false
           }
         }.to_struct
       end
@@ -273,20 +265,15 @@ module Git
         end
         config
       rescue IndexError
-        abort "configuration option not found"
+        abort 'configuration option not found'
       end
-
     end
 
     class Application < Sinatra::Base #:nodoc:
-
       set :project_root, lambda { Git::Webby.config.default.project_root }
       set :git_path,     lambda { Git::Webby.config.default.git_path }
 
-      mime_type :json, "application/json"
-
+      mime_type :json, 'application/json'
     end
-
   end
-
 end
